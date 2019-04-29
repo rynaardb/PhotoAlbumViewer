e9c9c905e6b81adf9835e2d0378ffc9ede0537f0
Rynaard Burger <rynaard.burger@xing.com>
2019-04-29 15:09:55 +0200

    Created basic navigation structured with initial Album View Controller

--- PhotoAlbumViewer/AppDelegate.swift
+++ PhotoAlbumViewer/AppDelegate.swift
@@ -15,12 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        window = UIWindow()
-        window?.backgroundColor = UIColor.cyan
-        window?.rootViewController = UIViewController()
-        window?.makeKeyAndVisible()
-
         return true
     }
 
