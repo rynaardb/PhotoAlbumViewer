34143d6b80d2fb405dcef9d09f8927be20a6e10f
Rynaard Burger <ryno.burger@gmail.com>
2019-05-01 12:27:07 +0200

    Completed implementation of Photos collection view

--- PhotoAlbumViewer/Photos/PhotoCell.swift
+++ PhotoAlbumViewer/Photos/PhotoCell.swift
@@ -1,17 +1,76 @@
 // PhotoCell.swift
 
 import UIKit
+import Kingfisher
 
 class PhotoCell: UICollectionViewCell {
-    public var viewModel: PhotoViewModel!
+    public var viewModel: PhotoViewModel! {
+        didSet {
+            prepareLayout()
+        }
+    }
+
+    var imageView: UIImageView!
+    var titleLabel: UILabel!
+
+    // MARK: Initializers
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-        self.contentView.backgroundColor = .blue
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: View Lifecycle
+
+    override func prepareForReuse() {
+        self.contentView.backgroundColor = .lightGray
+        imageView = nil
+        titleLabel.text = nil
+    }
+
+    // MARK: Private
+
+    private func prepareLayout() {
+        let options: KingfisherOptionsInfo = [.transition(.fade(0.5))]
+        imageView = UIImageView()
+        imageView.kf.setImage(with: viewModel.thumbnailURL, options: options)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        self.contentView.addSubview(imageView)
+
+        NSLayoutConstraint.activate([imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+                                     imageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+                                     imageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+                                     imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)])
+
+        let overlayView = UIView()
+        overlayView.backgroundColor = .black
+        overlayView.layer.opacity = 0.5
+        overlayView.translatesAutoresizingMaskIntoConstraints = false
+
+        imageView.addSubview(overlayView)
+
+        NSLayoutConstraint.activate([overlayView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+                                     overlayView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+                                     overlayView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor),
+                                     overlayView.heightAnchor.constraint(equalToConstant: 60.0)])
+
+        titleLabel = UILabel()
+        titleLabel.text = viewModel.title
+        titleLabel.numberOfLines = 4
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont.systemFont(ofSize: 11)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        overlayView.addSubview(titleLabel)
+
+        NSLayoutConstraint.activate([titleLabel.leadingAnchor.constraint(equalTo: overlayView.leadingAnchor, constant: 2.0),
+                                     titleLabel.trailingAnchor.constraint(equalTo: overlayView.trailingAnchor, constant: -2.0),
+                                     titleLabel.topAnchor.constraint(equalTo: overlayView.topAnchor, constant: 2.0),
+                                     titleLabel.centerXAnchor.constraint(equalTo: overlayView.centerXAnchor)])
     }
 }
--- PhotoAlbumViewer/Photos/PhotosViewController.swift
+++ PhotoAlbumViewer/Photos/PhotosViewController.swift
@@ -4,9 +4,18 @@ import UIKit
 
 private let reuseIdentifier = "PhotoCell"
 
-class PhotosViewController: UICollectionViewController {
+class PhotosViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     weak var coordinator: MainCoordinator?
     private var photos = [Photo]()
+    var cellSize = CGSize.zero
+
+    var layout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        return layout
+    }()
+
+    // MARK: Initializers
 
     convenience init() {
         self.init(photos: [Photo]())
@@ -14,25 +23,25 @@ class PhotosViewController: UICollectionViewController {
 
     init(photos: [Photo]) {
         self.photos = photos
-        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+        super.init(collectionViewLayout:layout)
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.collectionView!.register(PhotoCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        self.collectionView.backgroundColor = UIColor(hue: 0.63, saturation: 0.09, brightness: 0.19, alpha: 1.00)
+        self.title = "Photos"
     }
 
     // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return photos.count
@@ -48,35 +57,15 @@ class PhotosViewController: UICollectionViewController {
         return cell
     }
 
+    // MARK: UICollectionViewDelegateFlowLayout
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 150)
+    }
+
     // MARK: UICollectionViewDelegate
 
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // TODO: Wire up coordinator here
     }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
